c6b14d2b7483e99efb28f812bde1832254639049
@tottokotkd <tottokotkd@me.com>
2016-12-04 11:27:33 +0900

    add react component
    * just a copy of html :)

--- src/js/main.js
+++ src/js/main.js
@@ -12,7 +12,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'src/index.html'),
+    pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
